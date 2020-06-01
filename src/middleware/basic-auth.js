a00fc60bc22b91d5a2d51218eef9279c06469a73
Rise Erpelding <rise.erpelding@gmail.com>
2020-06-01 11:09:00 -0500

    ability to compare hashed passwords

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || ''
 
@@ -5,7 +7,6 @@ function requireAuth(req, res, next) {
   if (!authToken.toLowerCase().startsWith('basic ')) {
     return res.status(401).json({ error: 'Missing basic token' })
   } else {
-    console.log('hi')
     basicToken = authToken.slice('basic '.length, authToken.length)
   }
 
@@ -22,11 +23,18 @@ function requireAuth(req, res, next) {
   .where({ user_name: tokenUserName })
   .first()
   .then(user => {
-    if (!user || user.password !== tokenPassword) {
+    if (!user) {
       return res.status(401).json({ error: 'Unauthorized request' })
     }
-    req.user = user
-    next()
+    return bcrypt.compare(tokenPassword, user.password)
+      .then(passwordsMatch => {
+        if (!passwordsMatch) {
+          return res.status(401).json({ error: 'Unauthorized request' })
+        }
+
+        req.user = user
+        next()
+      })
   })
   .catch(next)
 }
