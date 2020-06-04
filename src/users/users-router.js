63e4c8c9b4a50854ee2923679fcb0409617c8eb3
Rise Erpelding <rise.erpelding@gmail.com>
2020-06-04 00:01:46 -0500

    added favorites-router from items in users-router

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -3,7 +3,7 @@ const UsersService = require('./users-service')
 const usersRouter = express.Router()
 const path = require('path')
 const jsonParser = express.json()
-// const { requireAuth } = require('../middleware/jwt-auth')
+const { requireAuth } = require('../middleware/jwt-auth')
 
 usersRouter
   .route('/:id')
@@ -41,41 +41,42 @@ usersRouter
       })
       .catch(next)
   })
-  .post(jsonParser, (req, res, next) => {
-    const { favorite_farm } = req.body
-    console.log(favorite_farm)
-    // Validation: get the farm and see if it exists, if not return a 404
-    // Validation: favorite_farm must be a number
-    //req.user.id instead of req.params.id
-    UsersService.addUserFavorite(
-      req.app.get('db'),
-      req.params.id,
-      favorite_farm
-    )
-      .then(favorite => {
-        res
-          .status(201)
-          .location(path.posix.join(
-            req.originalUrl,
-            favorite_farm.toString()
-          ))
-          .json(favorite)
-      })
-      .catch(next)
-  })
+//   .post(jsonParser, (req, res, next) => {
+//     const { favorite_farm, user_id } = req.body
+//     // console.log(favorite_farm)
+//     // Validation: get the farm and see if it exists, if not return a 404
+//     // Validation: favorite_farm must be a number
+//     //req.user.id instead of req.params.id
+//     console.log(req.user)
+//     UsersService.addUserFavorite(
+//       req.app.get('db'),
+//       user_id,
+//       favorite_farm
+//     )
+//       .then(favorite => {
+//         res
+//           .status(201)
+//           .location(path.posix.join(
+//             req.originalUrl,
+//             favorite_farm.toString()
+//           ))
+//           .json(favorite)
+//       })
+//       .catch(next)
+//   })
 
-usersRouter
-  .route('/:id/favorites/:farmId')
-  .delete((req, res, next) => {
-    UsersService.removeUserFavorite(
-      req.app.get('db'),
-      req.params.id,
-      req.params.farmId
-    )
-      .then(numRowsAffected => {
-        res.status(204).end()
-      })
-      .catch(next)
-  })
+// usersRouter
+//   .route('/:id/favorites/:farmId')
+//   .delete((req, res, next) => {
+//     UsersService.removeUserFavorite(
+//       req.app.get('db'),
+//       req.params.id,
+//       req.params.farmId
+//     )
+//       .then(numRowsAffected => {
+//         res.status(204).end()
+//       })
+//       .catch(next)
+//   })
 
   module.exports = usersRouter
